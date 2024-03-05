eabcbcee817ec0f86c577bf2a40b4d841426f1d2
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-03-05 00:37:17 +0000

    Teste

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,20 +1,28 @@
+var number1 = Math.floor(Math.random * 101)
+var number2 = Math.floor(Math.random * 101)
+
+// var add = number1 + number2;
+// var sub = number1 - number2;
+// var multiply = number1 * number2;
+// var div = number1 / number2;
+
 const mathAdd = require('../math');
-test(`O resultado é ${mathAdd}`, () => { 
-expect(mathAdd.add(2, 3)).toBe(5); 
+test(`O resultado ${number1} + ${number2} é:`, () => { 
+expect(mathAdd.add(number1, number2)).toBe(number1 + number2); 
 }); 
 
 const mathSub = require('../math');
-test(`O resultado é ${mathSub}`, () => { 
-expect(mathSub.subtract(9, 3)).toBe(6); 
+test(`O resultado ${number1} - ${number2} é:`, () => { 
+expect(mathSub.subtract(number1, number2)).toBe(number1 - number2); 
 }); 
 
 const mathMultiply = require('../math');
-test(`O resultado é ${mathMultiply}`, () => { 
-expect(mathMultiply.multiply(10, 10)).toBe(100); 
+test(`O resultado ${number1} * ${number2} é:`, () => { 
+expect(mathMultiply.multiply(number1, number2)).toBe(number1 * number2); 
 }); 
 
 const mathDivide = require('../math');
-test(`O resultado é ${mathDivide}`, () => { 
-expect(mathDivide.divide(8, 2)).toBe(4); 
+test(`O resultado ${number1} / ${number2} é:`, () => { 
+expect(mathDivide.divide(number1, number2)).toBe(number1 / number2); 
 }); 
 
